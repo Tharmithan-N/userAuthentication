9dff92692256a94711a7cc6e0fd6f55b872b8ce6
Tharmithan <tharmithan@orelit.com>
2024-08-08 20:14:36 +0530

    home component added

--- client/authentication/src/index.js
+++ client/authentication/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import SignUp from './pages/signUp';
+import Home from './pages/home';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +11,7 @@ root.render(
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
   </Router>
 );
